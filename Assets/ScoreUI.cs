76174eeb8146c1e70dc8c2741199c2de22120d98
Aman Sachan <aman9740@gmail.com>
2018-01-22 13:33:12 -0500

    Assets/GameplayScene.unity

--- Assets/ScoreUI.cs
+++ Assets/ScoreUI.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreUI : MonoBehaviour {
 
     Global globalObj;
-    GUIText scoreText;
+    public Text scoreText;
     // Use this for initialization
     void Start()
     {
         GameObject g = GameObject.Find("GlobalObject");
         globalObj = g.GetComponent<Global>();
-        scoreText = gameObject.GetComponent<GUIText>();
+        scoreText = gameObject.GetComponent<Text>();
     }
     // Update is called once per frame
     void Update()
