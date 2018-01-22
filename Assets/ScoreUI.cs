348eb1750f05ecfd7b193a89f37962b4bc8de85a
Anton Khabbaz <anton.khabbaz@gmail.com>
2018-01-21 22:17:49 -0500

    Now project works but no text working

---
+++ Assets/ScoreUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour {
+
+    Global globalObj;
+    GUIText scoreText;
+    // Use this for initialization
+    void Start()
+    {
+        GameObject g = GameObject.Find("GlobalObject");
+        globalObj = g.GetComponent<Global>();
+        scoreText = gameObject.GetComponent<GUIText>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = globalObj.score.ToString();
+    }
+}
